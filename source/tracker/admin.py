d3a164642ad3d51ecd8834c0a6f266edcadbb05c
Tilekbai <tilek009@mail.ru>
2021-03-11 01:05:15 +0600

    Модели:"Issue", "Status", "Issue_type" добавлены в админ-панель

--- source/tracker/admin.py
+++ source/tracker/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from tracker.models import Issue, Status, Issue_type
 
 # Register your models here.
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ["id", "summary", "description", "status", "issue_type", "created_at", "updated_at"]
+    list_filter = ["id", "summary", "status", "issue_type"]
+    search_fields = ["description", "created_at", "updated_at"]
+    fields = ["id", "summary", "description", "status", "issue_type", "created_at", "updated_at"]
+    readonly_fields = ["created_at", "updated_at", "id"]
+
+admin.site.register(Issue, IssueAdmin)
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ["id", "status_code"]
+    list_filter = ["id", "status_code"]
+    list_search = ["status_code"]
+    fields = ["id", "status_code"]
+    readonly_fields = ["id"]
+
+admin.site.register(Status, StatusAdmin)
+
+class Issue_typeAdmin(admin.ModelAdmin):
+    list_display = ["id", "issue_type_code"]
+    list_filter = ["id", "issue_type_code"]
+    list_search = ["issue_type_code"]
+    fields = ["id", "issue_type_code"]
+    readonly_fields = ["id"]
+
+admin.site.register(Issue_type, Issue_typeAdmin)

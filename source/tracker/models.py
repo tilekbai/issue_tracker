8839a058d9a266b35605c319c23b8cdabfd7410a
Tilekbai <tilek009@mail.ru>
2021-03-10 16:24:35 +0600

    Создана модель:"Issue"

--- source/tracker/models.py
+++ source/tracker/models.py
@@ -8,3 +8,18 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Issue(BaseModel):
+    summary = models.CharField(max_length = 60, null = False, blank = False, verbose_name = "Заголовок")
+    description = models.CharField(max_length = 2000, null = True, verbose_name = "Описание")
+    status = models.ForeignKey("tracker.status", on_delete=models.PROTECT, related_name="status", verbose_name="Статус")
+    issue_type = models.ForeignKey("models.Type", on_delete=models.PROTECT, related_name="issue_type", verbose_name="Тип_задачи")
+
+    class Meta:
+        db_table = "issues"
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+    
+    def __str__(self):
+        return f'{self.id}. {self.summary}: {self.status}, {self.issue_type}, {self.description}'

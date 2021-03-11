491c35b699288510b4d1a3894345368cb40dbe2c
Tilekbai <tilek009@mail.ru>
2021-03-11 18:54:11 +0600

    Соднаны Templates и Static

---
+++ source/tracker/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from issue.models import Issue
+
+
+class IssueForm(forms.ModelForm):
+    """
+    Форма для создания и редактирваония объектов статьи
+    https://docs.djangoproject.com/en/3.1/ref/forms/
+    """
+    class Meta:
+        model = Issue
+        fields = ('title', 'status', 'description', 'summary', 'issue_type')
+
+
+class IssueDeleteForm(forms.Form):
+    title = forms.CharField(max_length=120, required=True, label='Введите название статьи, чтобы удалить её')
--- source/tracker/views.py
+++ source/tracker/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
+from tracker.models import Issue
 
 # Create your views here.
+
+class IndexView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        issues = Issue.objects.all()
+        return render(request, 'index.html', context={'issues': issues})

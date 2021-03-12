3980b6a6eb3ee64b491a4767d5b461a6509dad73
Tilekbai <tilek009@mail.ru>
2021-03-13 02:53:13 +0600

    Добавлены ссылки и шаблоны на страницах html

--- source/tracker/forms.py
+++ source/tracker/forms.py
@@ -4,13 +4,11 @@ from tracker.models import Issue
 
 
 class IssueForm(forms.ModelForm):
-    """ 
-    Форма для создания и редактирваония объектов статьи
-    https://docs.djangoproject.com/en/3.1/ref/forms/
-    """
     class Meta:
         model = Issue
         fields = ('summary', 'description', 'status', 'issue_type')
+
+
 
 
 class IssueDeleteForm(forms.Form):
